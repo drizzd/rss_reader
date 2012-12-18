05c7464365ed78637b60fbd8776c02d399e1e5b9
Clemens Buchacher <drizzd@aon.at>
2012-12-18 09:18:29 +0100

    use List<Map> instead of Cursor

--- src/at/drizzd/rss/rss.java
+++ src/at/drizzd/rss/rss.java
@@ -2,26 +2,41 @@ package at.drizzd.rss;
 
 import android.app.ListActivity;
 import android.os.Bundle;
-import android.database.MatrixCursor;
-import android.widget.SimpleCursorAdapter;
+import android.widget.SimpleAdapter;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
 
 public class rss extends ListActivity
 {
+    private HashMap<String, String> mapOneEntry(String column, String row)
+    {
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put(column, row);
+        return map;
+    }
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
 
+        String[] entries = {
+            "a", "b", "c", "d", "e", "f", "g",
+            "h", "i", "j", "k", "l", "x", "y", "z"
+        };
         String column = "asdf";
-        MatrixCursor cursor = new MatrixCursor(new String[] {column});
-        cursor.addRow(new Object[] {"x"});
-        cursor.addRow(new Object[] {"y"});
-        cursor.addRow(new Object[] {"z"});
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(
+        ArrayList<Map<String, String>> list = new ArrayList<Map<String, String>>();
+        for (int i = 0; i < entries.length; i++) {
+            list.add(mapOneEntry(column, entries[i]));
+        }
+
+        SimpleAdapter adapter = new SimpleAdapter(
                 this,
+                list,
                 android.R.layout.simple_list_item_1,
-                cursor,
                 new String[] {column},
                 new int[] {android.R.id.text1});
         setListAdapter(adapter);
