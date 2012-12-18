82d0d699c959f27e90861d80d1dd60a5b68b21a7
Clemens Buchacher <drizzd@aon.at>
2012-12-18 08:49:14 +0100

    create a basic list view

--- src/at/drizzd/rss/rss.java
+++ src/at/drizzd/rss/rss.java
@@ -1,15 +1,29 @@
 package at.drizzd.rss;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.database.MatrixCursor;
+import android.widget.SimpleCursorAdapter;
 
-public class rss extends Activity
+public class rss extends ListActivity
 {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+
+        String column = "asdf";
+        MatrixCursor cursor = new MatrixCursor(new String[] {column});
+        cursor.addRow(new Object[] {"x"});
+        cursor.addRow(new Object[] {"y"});
+        cursor.addRow(new Object[] {"z"});
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(
+                this,
+                android.R.layout.simple_list_item_1,
+                cursor,
+                new String[] {column},
+                new int[] {android.R.id.text1});
+        setListAdapter(adapter);
     }
 }
